bd0257c92b297227781a610a8d808c1a742112a9
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-04-22 09:28:30 +1000

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '@xelene/tgui/dist/styles.css';
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
